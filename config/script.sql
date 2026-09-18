f0da9d605d69ee50a997467db0b966c011012d36
HyperFire7067 <super.fire.9000.mega.d@gmail.com>
2026-09-18 15:15:04 +0000

    Criacão da database script

--- config/script.sql
+++ config/script.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `favorito`
 --
 
+CREATE DATABASE script;
+USE DATABASE script;
+
 DROP TABLE IF EXISTS `favorito`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
